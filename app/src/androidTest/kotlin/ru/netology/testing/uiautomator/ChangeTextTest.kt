416548920e499784ce78591b1ed3103eb51d1a6d
Svetlana Alekseeva <lanaleks90gmail.com>
2024-06-27 20:02:16 +0400

    Time change 2

--- app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
+++ app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
@@ -130,7 +130,7 @@ class ChangeTextTest {
 
             device.findObject(By.res(packageName, "userInput")).text = textToSet
             device.findObject(By.res(packageName, "buttonActivity")).click()
-            Thread.sleep(5000)
+            Thread.sleep(10000)
 
             val result = device.findObject(By.res(packageName, "text")).text
             assertEquals(result, textToSet)
